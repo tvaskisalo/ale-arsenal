c1ae8766ea9094f68e7467d876fe4c97773cd6b0
tvaskisalo <tapani.vaskisalo@outlook.com>
2023-09-04 02:04:07 +0300

    Ping backend from frontend

--- ale-arsenal-frontend/src/App.tsx
+++ ale-arsenal-frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const [msg, setMsg] = useState('')
+  const pingBackend = async (e: React.MouseEvent) => {
+    e.preventDefault()
+    const message = await fetch('http://0.0.0.0:8080/')
+    const text = await message.text()
+    setMsg(text)
+  }
   return (
     <>
       <div>
@@ -28,6 +34,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <button onClick={pingBackend}>PING</button>
+      <div>{msg}</div>
     </>
   )
 }
